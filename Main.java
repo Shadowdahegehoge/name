d6f1ae14a873ac544e8a93b9244c186e25bd7aab
Shadowdahegehoge <165925684+Shadowdahegehoge@users.noreply.github.com>
2024-04-18 07:47:08 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+public static void main(String [] args ) {
+Scanner input =  new Scanner( System.in );
+ int life =3;
+  while( life > 0 ){
+System.out.print("Shadow"); 
+    life--;
+  }
+}
+}
